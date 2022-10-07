246abd7a313fa277b1633ef25a6d7ad219670880
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2022-10-06 23:16:51 -0600

    Update model.py

--- Project2/Code/model.py
+++ Project2/Code/model.py
@@ -33,92 +33,105 @@ class Model:
         for contents in folds[other_folds]:
           training_data.append(contents)
 
-    training_df = pd.DataFrame()
+      training_df_with_class = pd.DataFrame()
 
-    testing_df = pd.DataFrame()
-    
-    for count1, i in enumerate(training_data):
-      temp_df = pd.DataFrame(i)
-      temp_df_T = temp_df.transpose()
-      print(temp_df_T)
-      training_df = training_df.append(temp_df_T)
-      print(training_df)
+      testing_df = pd.DataFrame()
       
+      for count1, i in enumerate(training_data):
+        temp_df = pd.DataFrame(i)
+        temp_df_T = temp_df.transpose()
+        print(temp_df_T)
+        training_df_with_class = training_df_with_class.append(temp_df_T)
+        training_df_with_class = training_df_with_class.drop(columns=['ID Number'])
+        print(training_df)
 
-    #for count2, ii in enumerate(testing_data):
-     # testing_df.iloc[count2] = ii
+      for count1, i in enumerate(testing_data):
+        temp_df = pd.DataFrame(i)
+        temp_df_T = temp_df.transpose()
+        print(temp_df_T)
+        testing_df = testing_df.append(temp_df_T)
+        testing_df = testing_df.drop(columns=['ID Number'])
+        print(training_df)
 
-    #print(folds)
-    print(training_df)
-    print(testing_df)
-      #for allof in all_folds:
-      #  for peices in 
-
-
-
-    #all_folds = range(10)
-
-    
-
-    #training = folds[1]
-
-
-
-
-
-    #df = pd.read_csv('https://github.com/IsaacBoyd2/ActualFactualML/blob/main/Project2/Data/glass.csv?raw=true')
-
-    '''df_matrix = pd.DataFrame(np.nan, index=range(len(df)), columns = range(len(df)))
-
-
-    df = df.drop(0)
-    df = df.reset_index()
-    df2 = df.iloc[: , 2:-1]
-
-    decision = []
-
-    for count1 in range(len(df2)):
-        base = df2.iloc[count1]
-        for count2 in range(len(df2)):
-          dist1 = []
-          for count3 in range(len(df2.columns)):
-            dist1.append((base[count3] - df2.iloc[count2][count3])**2)
-          
-          summation = sum(dist1)
-          distance = np.sqrt(summation)
-
-          df_matrix.loc[count1, count2] = distance 
-
-        comparison_array = np.array(df_matrix.loc[count1])
-        k = k_nn+1
-        index = np.argpartition(comparison_array, k)
-        reduced_idx = index[:k]
-
-        reduced_list = reduced_idx.tolist()
-
-        reduced_list.remove(count1)
-
-        majority =[]
-        for i in reduced_list:
-          #print(df['Class'][i])
-          majority.append(df['Class'][i])
-
-        class_decision = st.mode(majority)
+      training_df = training_df_with_class.copy
+      training_df = training_df.drop(columns=['Class'])
+      testing_df = testing_df.drop(columns=['Class'])
         
 
-        #print(class_decision[0])
-        decision.append(class_decision)
+      #for count2, ii in enumerate(testing_data):
+      # testing_df.iloc[count2] = ii
 
-    counts = 0
-    for i in range(len(decision)):
-      #print(decision[i][0])
-      #print(df['Class'][i])
-      if decision[i][0] == df['Class'][i]:
-        counts += 1
-
-    print(counts)
-    print(counts/len(df))
+      #print(folds)
+      print(training_df)
+      print(testing_df)
+        #for allof in all_folds:
+        #  for peices in 
 
 
-    return [self.tuning, self.labels]'''
+
+      #all_folds = range(10)
+
+      
+
+      #training = folds[1]
+
+
+
+
+
+      #df = pd.read_csv('https://github.com/IsaacBoyd2/ActualFactualML/blob/main/Project2/Data/glass.csv?raw=true')
+
+      df_matrix = pd.DataFrame(np.nan, index=range(len(training_df)), columns = range(len(training_df)))
+
+
+      #df = df.drop(0)
+      #df = df.reset_index()
+      #df2 = df.iloc[: , 2:-1]
+
+      decision = []
+
+      for count1 in range(len(testing_df)):
+          base = testing_df.iloc[count1]
+          for count2 in range(len(training_df)):
+            dist1 = []
+            for count3 in range(len(testing_df.columns)):
+              dist1.append((base[count3] - training_df.iloc[count2][count3])**2)
+          
+            summation = sum(dist1)
+            distance = np.sqrt(summation)
+
+            df_matrix.loc[count1, count2] = distance 
+
+          comparison_array = np.array(df_matrix.loc[count1])
+          k = k_nn+1
+          index = np.argpartition(comparison_array, k)
+          reduced_idx = index[:k]
+
+          reduced_list = reduced_idx.tolist()
+
+          reduced_list.remove(count1)
+
+          majority =[]
+          for i in reduced_list:
+            #print(df['Class'][i])
+            majority.append(training_df_with_class['Class'][i])
+
+          class_decision = st.mode(majority)
+          
+
+          #print(class_decision[0])
+          decision.append(class_decision)
+
+      counts = 0
+      for i in range(len(decision)):
+        #print(decision[i][0])
+        #print(df['Class'][i])
+        if decision[i][0] == training_df_with_class['Class'][i]:
+          counts += 1
+
+      print(counts)
+      print(counts/len(training_df_with_class))
+
+
+      return [self.tuning, self.labels]
   
