fbb8465f23e6d1f8cfe7c552ddf77a9689d181f0
JamesLucas943 <56455079+JamesLucas943@users.noreply.github.com>
2022-10-09 17:09:43 -0600

    Update loss.py

--- Project2/Code/loss.py
+++ Project2/Code/loss.py
@@ -18,15 +18,17 @@ class Loss:
 
   #initialization
   def __init__ (self):
-    F1 = []
+    F1 = 0
     precision = []
     recall = []
     error = []
   
   def calculate(self, value, classes, pred, facts):
+    #makes a confusion Matrix that is the correct size
     confusionMat = np.zeros([len(classes),len(classes)])
+
     if value == 0:
-      #Creates a Confusion Matrix that will be used for precision, recall, and F1 calculations.
+      #populates the confusion matrix that will be used for precision, recall, and F1 calculations.
       for i in range(len(facts)):
         for j in range(len(facts[i])):
           indHorz = 0
@@ -41,7 +43,32 @@ class Loss:
           #adds an occurance in the confusion matrix
           confusionMat[indHorz, indVert] += 1
 
-      print("\n\n\n\nConfusionMat: ", confusionMat)
+      #calculates precision and recall
+      for i in range(len(confusionMat)):
+        truePos = 0
+        falsePos = 0
+        falseNeg = 0
+        for j in range(len(confusionMat[i])):
+          if i == j:
+            truePos = confusionMat[i][j]
+          else:
+            falsePos = falsePos + confusionMat[i][j]
+            falseNeg = falseNeg + confusionMat[j][i]
+          
+
+        self.precison.append(truePos/(truePos+falsePos))
+        self.recall.append(truePos/(truePos+falseNeg))
+
+      #calculates the F1
+      avgPrec = 0
+      avgRec = 0
+      for i in range(len(self.precision)):
+        avgPrec = avgPrec + self.precision[i]
+        avgRec = avgRec + self.recall[i]
+      avgPrec = avgPrec/len(self.precision)
+      avgRec = avgRec/len(self.recall)
+
+      self.F1 = 2*((avgPrec*avgRec)/(avgPrec+avgRec))
 
     else:
       pass
