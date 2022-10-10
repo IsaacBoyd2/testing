7bd40dbb9e876811e32c2ea3eb49741d17f20c0c
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2022-10-09 18:31:03 -0600

    Update model_regression.py

--- Project2/Code/model_regression.py
+++ Project2/Code/model_regression.py
@@ -23,6 +23,9 @@ class Model:
     #now we are going to run each fold and hold out one.
 
     accuracies = []
+    thing3 = []
+    thing4 =[]
+
 
     for iterations in range(10):
       all_folds = [0,1,2,3,4,5,6,7,8,9]
