889e75b0f765de8465d12381304c8717b99445a9
IsaacBoyd2 <97537554+IsaacBoyd2@users.noreply.github.com>
2022-10-11 09:18:09 -0600

    Update preprocessing.py

--- Project2/Code/preprocessing.py
+++ Project2/Code/preprocessing.py
@@ -18,10 +18,10 @@ class Preprocessing:
 
   #initialization
   def __init__ (self):
-    self.df = pd.DataFrame
-    self.folds = []
-    self.tuning = []
-    self.value = int()
+    df = pd.DataFrame
+    folds = []
+    tuning = []
+    value = int()
 
   #method that allows the user to chose the dataset, and preprocesses that said data set.
   def process(self):
@@ -55,7 +55,7 @@ class Preprocessing:
     elif DataNumber == '3':
       df = pd.read_csv('https://github.com/IsaacBoyd2/ActualFactualML/blob/main/Project2/Data/soybean-small.csv?raw=true')
       print("Using Soybean data (Classification)")
-      self.value = 0
+      self.value = 6
       self.df = df
 
     elif DataNumber == '4':
@@ -269,6 +269,6 @@ class Preprocessing:
 
       self.folds = foldTotal
 
-#preProcess = Preprocessing()
-#preProcess.process()
-#preProcess.fold()
+preProcess = Preprocessing()
+preProcess.process()
+preProcess.fold()
